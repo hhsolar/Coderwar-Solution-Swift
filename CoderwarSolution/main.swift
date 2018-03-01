bda458d8364fbefec04f1186ef8b4cf011957d80
hhsolar <hongh17717@gmail.com>
2018-03-01 00:36:06 -0800

    Initial Commit

---
+++ CoderwarSolution/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CoderwarSolution
+//
+//  Created by apple on 1/3/2018.
+//  Copyright © 2018 greatwall. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
